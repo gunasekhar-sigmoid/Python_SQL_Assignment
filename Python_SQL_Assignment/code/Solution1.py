3277209410aaaf6e5ebc35b29f05b50dff529a65
gunasekhar-sigmoid <88651678+gunasekhar-sigmoid@users.noreply.github.com>
2021-08-09 16:14:41 +0530

    Add files via upload

---
+++ Python_SQL_Assignment/code/Solution1.py
@@ -0,0 +1,48 @@
+import psycopg2
+import pandas as pd
+import logging
+
+class employees:
+
+    def emp(self):
+
+        try:
+            #establishing a connection with postgreSQL
+            connection = psycopg2.connect(
+            database="assignment",
+            user="postgres",
+            password="root")
+            cursor = connection.cursor()
+
+            #running the query for required output
+            query = """
+            SELECT e1.empno, e1.ename, (case when mgr is not null then (select ename from emp as e2 where e1.mgr=e2.empno limit 1) else null end) as manager
+            from emp as e1 
+            """
+            cursor.execute(query)
+
+            #collecting the data obtained by executing the query
+            columns = [desc[0] for desc in cursor.description]
+            data = cursor.fetchall()
+            df = pd.DataFrame(list(data), columns=columns)
+
+            #storing the data collecting in a xlsx file
+            writer = pd.ExcelWriter('solution_1.xlsx')
+            df.to_excel(writer, sheet_name='bar')
+            writer.save()
+
+        except Exception as e:
+            logging.error("Error", e)
+
+        finally:
+            #closing the connection between postgreSQL and python
+            if connection is not None:
+                cursor.close()
+                connection.close()
+
+
+if __name__ == '__main__':
+ connection = None
+ cur = None
+ employee = employees()
+ employee.emp()
